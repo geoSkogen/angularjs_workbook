55ebf2e5e369888b239631b13f50ba929ac2c6d4
joseph_scoggins <borninthecaul@gmail.com>
2017-01-07 21:56:14 -0800

    fixes weird error

--- app/app.module.js
+++ app/app.module.js
@@ -1,3 +1,3 @@
 //this would be fed to the dependencies array of the main app, eg:
 //var mainApp = angular.module("mainApp", ["listCompApp"]);
-angular.module("listCompApp", ['ngRoute', 'albumList', 'albumDetails']);
+angular.module("listCompApp", ['ngRoute', 'core', 'albumList', 'albumDetails']);
